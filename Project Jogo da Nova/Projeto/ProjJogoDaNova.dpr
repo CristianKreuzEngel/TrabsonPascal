3e41b599dae5616d5c0149cdb8ff492c660d0dd6
Cristian Kreuz Engel <108903664+CristianKreuzEngel@users.noreply.github.com>
2023-05-05 19:47:15 -0300

    alterações

---
+++ Project Jogo da Nova/Projeto/ProjJogoDaNova.dpr
@@ -0,0 +1,14 @@
+program ProjJogoDaNova;
+
+uses
+  Vcl.Forms,
+  unProjectJogoNova in 'unProjectJogoNova.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
